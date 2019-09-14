0b49c959139a91fcd20eae2edb32c592eb97fa33
vanthuan0306 <55068541+vanthuan0306@users.noreply.github.com>
2019-09-14 22:16:21 +0700

    Tuần 1 Lab 2

---
+++ Bai1.java
@@ -0,0 +1,24 @@
+package Tuan1Lab2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		double a, b;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập hệ số a: ");
+		a = scanner.nextDouble();
+		System.out.print("Nhập hằng số b: ");
+		b = scanner.nextDouble();
+		if (a == 0) {
+			if (b == 0) {
+				System.out.println("Phương trình vô số nghiệm");
+			} else {
+				System.out.println("Phương trình vô nghiệm");
+			}
+		} else {
+			System.out.println("Nghiệm của phương trình là: " + -b / a);
+		}
+	}
+}
---
+++ Bai3.java
@@ -0,0 +1,20 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Bai3 {
+
+	public static void main(String[] args) {
+		double soDien;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhập số điện: ");
+		soDien = scanner.nextDouble();
+
+		if (soDien < 50) {
+			System.out.println("Tiền điện của quý khách là: " + soDien * 1000);
+		}
+		else {
+			System.out.println("Tiền điện của quý khách là: " + soDien * 1200);
+		}
+	}
+}
