d740f333ee9513ca3d38848e38c4c9234e7337b5
vanthuan0306 <vanthuan0306@gmail.com>
2019-10-14 18:35:59 +0700

    Tuần 6

---
+++ DHPM11/src/Tuan4/BooksSuppliers.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tuan4;
+
+/**
+ *
+ * @author VanThuan
+ */
+public class BooksSuppliers {
+    private int supplierid;
+    private String suppliername;
+    private String suppliertype;
+    private String address;
+    private String contactno;
+    private String emailid;
+    private String remarks;
+    public BooksSuppliers(){
+        
+    }
+    public BooksSuppliers(String suppliername, String suppliertype, String address, String contactno, String emailid, String remarks){
+        this.suppliername = suppliername;
+        this.suppliertype = suppliertype;
+        this.address = address;
+        this.contactno = contactno;
+        this.emailid = emailid;
+        this.remarks = remarks;
+    }
+
+    public int getSupplierid() {
+        return supplierid;
+    }
+
+    public String getSuppliername() {
+        return suppliername;
+    }
+
+    public String getSuppliertype() {
+        return suppliertype;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getContactno() {
+        return contactno;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setSupplierid(int supplierid) {
+        this.supplierid = supplierid;
+    }
+
+    public void setSuppliername(String suppliername) {
+        this.suppliername = suppliername;
+    }
+
+    public void setSuppliertype(String suppliertype) {
+        this.suppliertype = suppliertype;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setContactno(String contactno) {
+        this.contactno = contactno;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+    
+}
---
+++ DHPM11/src/dhpm/tuan6/Employee.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhpm.tuan6;
+
+
+/**
+ *
+ * @author VanThuan
+ */
+public class Employee {
+    private  int id;
+    private String name;
+    private String address;
+    private String gender;
+    private String knowledge;
+    private String subject;
+    public Employee(){
+        
+    }
+
+    public Employee(String name, String address, String gender, String knowledge, String subject) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.gender = gender;
+        this.knowledge = knowledge;
+        this.subject = subject;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getKnowledge() {
+        return knowledge;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setKnowledge(String knowledge) {
+        this.knowledge = knowledge;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+    
+}
